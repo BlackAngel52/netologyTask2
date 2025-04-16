b5a9b311c6ceec5366827f32dc43efe3af537a28
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-04-17 00:58:58 +0300

    Add files via upload

---
+++ netologyTask2_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int inputNum = { };
+    std::cout << "Введите число:" << "\n";
+    std::cin >> inputNum;
+    std::cout << "Вы ввели: " << "\n";
+    std::cout << inputNum;
+}
+
+
---
+++ netologyTask2_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите слово: \n";
+    std::string inputWord = {};
+    std::getline(std::cin >> std::ws, inputWord);
+    std::cout << "Вы ввели: \n";
+    std::cout << inputWord;
+
+}
+
+
